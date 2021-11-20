84b1dacc21aae9a585ad37d27a4c5031bb99440a
mjms0214 <mjms0214@sookmyung.ac.kr>
2021-11-20 17:47:37 +0900

    init

---
+++ server.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//const MongoClient = require('mongodb').MongoClient;
+//MongoClient.connect('mongodb+srv://admin:health123@cluster0.g6wfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(err, client){
+    
+//})
+
+app.listen(8080, function(){
+        console.log('listening on 8080');
+    });
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/views/main/main_before_login.html');
+});
+
+app.get('/main', function(req, res){
+    res.sendFile(__dirname+'/views/main/main_after_login.html');
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(__dirname+'/views/main/login.html');
+});
+
+app.get('/join', function(req, res){
+    res.sendFile(__dirname+'/views/main/join.html');
+});
+
+app.get('/info', function(req, res){
+    res.sendFile(__dirname+'/views/main/info_before_login.html');
+});
+
+app.get('/health', function(req, res){
+    res.sendFile(__dirname+'/views/main/health_before_login.html');
+});
+
+app.get('/community_main', function(req, res){
+    res.sendFile(__dirname+'/views/main/communitymain_before_login.html');
+});
+
+app.get('/challenge_main', function(req, res){
+    res.sendFile(__dirname+'/views/main/challengemain_before_login.html');
+});
+
+app.get('/hometraining_main', function(req, res){
+    res.sendFile(__dirname+'/views/main/hometraining_before_login.html');
+});
+
+app.get('/info2', function(req, res){
+    res.sendFile(__dirname+'/views/main/info_after_login.html');
+});
+
+app.get('/health2', function(req, res){
+    res.sendFile(__dirname+'/views/main/health_after_login.html');
+});
+
+app.get('/hometraining_main2', function(req, res){
+    res.sendFile(__dirname+'/views/main/hometraining_after_login.html');
+});
+
+
+
+app.get('/community', function (req, res) {
+    res.render('community/comm_list.ejs');
+});
+
+app.get('/comm_free', function (req, res) {
+    res.render('community/comm_free.ejs');
+});
+
+app.get('/comm_body', function (req, res) {
+    res.render('community/comm_body.ejs');
+});
+
+app.get('/comm_crew', function (req, res) {
+    res.render('community/comm_crew.ejs');
+});
+
+app.get('/comm_tips', function (req, res) {
+    res.render('community/comm_tips.ejs');
+});
+
+app.get('/comm_meal', function (req, res) {
+    res.render('community/comm_meal.ejs');
+});
+
+app.get('/comm_go', function (req, res) {
+    res.render('community/comm_go.ejs');
+});
+
+app.get("/challenge/introduce", function (req, res) {
+    res.sendFile(__dirname+"/views/challenge/challenge-introduce.html");
+});
+
+app.get("/challenge/certification", function (req, res) {
+    res.sendFile(__dirname+"/views/challenge/challenge-certification.html");
+});
+
+app.get("/challenge/payment", function (req, res) {
+    res.sendFile(__dirname+"/views/challenge/challenge-payment.html");
+});
+
+app.get("/challenge/stamp", function (req, res) {
+    res.sendFile(__dirname+"/views/challenge/challenge-stamp.html");
+});
